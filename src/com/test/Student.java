9b279bc760d50284b6ae1646215bec0f8c52ef8a
KajalRithe261 <kajal260896@gmail.com>
2021-06-25 10:55:28 +0530

    Demo to add StudentApp

---
+++ src/com/test/Student.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Student {
+
+	
+	private int id;
+	private String name;
+	
+	public void getIdNameInfo() {
+		System.out.println("Student id is : " + id);
+		System.out.println("Student Name is : " + name);
+	}
+}
