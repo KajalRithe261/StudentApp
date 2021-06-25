379a952fd3ba6c408e705aee8d5b8d6a0e7d11b5
KajalRithe261 <kajal260896@gmail.com>
2021-06-25 11:18:50 +0530

    Add StudentAppDemo class

---
+++ src/com/test/StudentAppDemo.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class StudentAppDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am in main class");
+		
+		Student s = new Student();
+		s.getIdNameInfo();
+	}
+
+}
